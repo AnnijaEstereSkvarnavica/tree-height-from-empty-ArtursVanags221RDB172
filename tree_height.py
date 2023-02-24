512b67d15b305e5fedee06317ac3c29ed0d7315a
ArtursVanags221RDB172 <124793162+ArtursVanags221RDB172@users.noreply.github.com>
2023-02-24 23:03:06 +0000

    tried to finish

--- tree_height.py
+++ tree_height.py
@@ -6,20 +6,63 @@ import threading
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
+    kur = [[] for _ in range(n)]
+    root = None
+    #max_height = 0
     # Your code here
-    return max_height
+    for g, parent in enumerate(parents):
+        if parent == -1:
+            root = g
+           
+        else:
+            kur[parent].append(g)
 
+    def max_height(vuzol):
+        height = 1
+        
+        if not kur[vuzol]:
+            return height
+        else:
+            for child in kur[vuzol]:
+                height = max(height, max_height(child))
+
+            return height + 1
+    return max_height(root)
 
 def main():
     # implement input form keyboard and from files
-    
+    text = input()
+    if "I" in text:
+        # input number of elements
+        n = int(input())
+        # input values in one variable, separate with space, split these values in an array
+        parents = list(map(int, input().split()))
+    elif "F" in text:
+
     # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
+    # account for github input inprecision 
+        path = './test/'
+        file = input()
+        folder = path + file
+        if "a" not in file:
+            try:
+                with open(folder) as f:
+                    n = int(f.readline())
+                    parents = list(map(int, f.readline().split()))
+            except Exception as e:
+                print("Kluda:(", str(e))
+                return
+            
+        else:
+            print("Kluda")
+            return
+        
+    else:
+        print("Enter 'I' or 'F':")
+        return
+            
     # call the function and output it's result
+    print(compute_height(n, parents))
 
 
 # In Python, the default limit on recursion depth is rather low,
